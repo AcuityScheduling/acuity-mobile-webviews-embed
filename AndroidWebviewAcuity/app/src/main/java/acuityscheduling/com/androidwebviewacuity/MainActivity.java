6a13dacee1ded0cab89a04224af33f3bcdb3c1e8
John Kokkinidis <sudoplz@gmail.com>
2016-10-20 18:15:40 +0300

    - Replaced the dancefuerte account with the example account for Android.

--- AndroidWebviewAcuity/app/src/main/java/acuityscheduling/com/androidwebviewacuity/MainActivity.java
+++ AndroidWebviewAcuity/app/src/main/java/acuityscheduling/com/androidwebviewacuity/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = scheduler.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        scheduler.loadData("<iframe src=\"https://app.acuityscheduling.com/schedule.php?owner=12808744\" width=\"100%\" height=\"800\" frameBorder=\"0\"></iframe>\n" +
+        scheduler.loadData("<iframe src=\"https://app.acuityscheduling.com/schedule.php?owner=11266122\" width=\"100%\" height=\"800\" frameBorder=\"0\"></iframe>\n" +
                         "<script src=\"https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js\" type=\"text/javascript\"></script>", "text/html",
                 "utf-8");
 
